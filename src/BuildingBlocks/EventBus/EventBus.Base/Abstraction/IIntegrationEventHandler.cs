2a7eb6cbacc6936969789bf59a015be6a2530a20
oaslan <oaslan@innova.com.tr>
2022-11-10 23:45:27 +0300

    event bus classes written.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using EventBus.Base.Events;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<TIntegrationEvent> : IntegrationEventHandler 
+        where TIntegrationEvent: IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+
+    }
+}
